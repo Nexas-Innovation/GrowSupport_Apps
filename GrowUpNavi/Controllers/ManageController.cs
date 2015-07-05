e49f7b4adda5c510d07afb0d62ff6e9423abb8ed
dev-kanage <developer.kanage0127@gmail.com>
2015-07-05 12:10:20 +0900

    アーキテクト設計を実装に反映＆jQueryUIのインストール

---
+++ GrowUpNavi/Controllers/ManageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrowUpNavi.Controllers
+{
+    public class ManageController : Controller
+    {
+        // GET: Manage
+        public ActionResult Master()
+        {
+            return View();
+        }
+    }
+}
---
+++ GrowUpNavi/ViewModels/Info/MenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrowUpNavi.ViewModels.Info
+{
+    public class MenuViewModel
+    {
+    }
+}
