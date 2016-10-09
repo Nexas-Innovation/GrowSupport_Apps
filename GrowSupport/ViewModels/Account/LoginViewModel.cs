faddd951b7da01ee4a9a27fd0e9757a42fe0aa11
dev-kanage <developer.kanage0127@gmail.com>
2016-10-09 11:59:25 +0900

    ログイン画面作成途中

--- GrowSupport/ViewModels/Account/LoginViewModel.cs
+++ GrowSupport/ViewModels/Account/LoginViewModel.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GrowUpNavi.ViewModels.Account
 {
+    /// <summary>
+    /// ログイン画面のビューモデル
+    /// </summary>
     public class LoginViewModel
     {
+        /// <summary>
+        /// ID（メールアドレス）
+        /// </summary>
+        [DataType(DataType.EmailAddress)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
